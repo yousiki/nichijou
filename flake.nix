02bf9d07119b16d37ef8577aff7207220054185d
yousiki <you.siki@outlook.com>
2025-06-17 16:22:26 +0800

    Add nixConfig with binary cache substituters
    Configures multiple Cachix substituters and Chinese mirrors for faster
    package downloads, with corresponding trusted public keys.

--- flake.nix
+++ flake.nix
@@ -51,4 +51,28 @@
         formatter = import ./formatter.nix { inherit self inputs channels; };
       };
     };
+
+  nixConfig = rec {
+    substituters = [
+      # Official cache server.
+      "https://cache.nixos.org" # priority=40
+      # Additional cache servers.
+      "https://nichijou.cachix.org" # priority=41
+      "https://nix-community.cachix.org" # priority=41
+      "https://numtide.cachix.org" # priority=41
+      "https://catppuccin.cachix.org" # priority=41
+      # Mirrors for cache.nixos.org in China.
+      "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store?priority=39"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store?priority=38"
+      "https://mirrors.ustc.edu.cn/nix-channels/store?priority=39"
+    ];
+    trusted-substituters = substituters;
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="
+      "nichijou.cachix.org-1:rbaTU9nLgVW9BK/HSV41vsag6A7/A/caBpcX+cR/6Ps="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
+  };
 }
