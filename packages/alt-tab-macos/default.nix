37cbdd6fc14474567a8ff7b174e0b0c75b1de84a
yousiki <18757689+yousiki@users.noreply.github.com>
2025-08-01 01:20:15 +0000

    chore: update packages

--- packages/alt-tab-macos/default.nix
+++ packages/alt-tab-macos/default.nix
@@ -2,12 +2,12 @@
 pkgs.alt-tab-macos.overrideAttrs (
   finalAttrs: _prevAttrs: {
     name = "alt-tab";
-    version = "7.25.0";
+    version = "7.26.0";
 
     src = fetchurl {
       name = "AltTab-${finalAttrs.version}.zip";
       url = "https://github.com/lwouis/alt-tab-macos/releases/download/v${finalAttrs.version}/AltTab-${finalAttrs.version}.zip";
-      hash = "sha256-e13en0fQHO0i49gP1zU6ms9TDMAwo1qsubsTi/DdIUo=";
+      hash = "sha256-tDy+GFZw9hD2kelPOJioRvcmbPZ9bQu+IRDBEOamsJs=";
     };
   }
 )
