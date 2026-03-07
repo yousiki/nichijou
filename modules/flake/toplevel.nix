16bab0868a2a608f87182c7691443dc70535f0fa
yousiki <you.siki@outlook.com>
2026-03-07 22:55:42 +0900

    Drop x86_64-darwin flake support

--- modules/flake/toplevel.nix
+++ modules/flake/toplevel.nix
@@ -1,8 +1,18 @@
 # Top-level flake glue to get our configuration working
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
     inputs.nixos-unified.flakeModules.default
     inputs.nixos-unified.flakeModules.autoWire
+  ];
+
+  systems = lib.mkForce [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-linux"
   ];
 
   perSystem = {self', ...}: {
