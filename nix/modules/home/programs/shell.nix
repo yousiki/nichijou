57d419ce4012f9e98a550759cc41f53b03ceea5e
yousiki <you.siki@outlook.com>
2026-05-26 21:12:03 +0900

    Let Catppuccin configure Starship

--- nix/modules/home/programs/shell.nix
+++ nix/modules/home/programs/shell.nix
@@ -36,12 +36,5 @@
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
-    settings = {
-      add_newline = false;
-      character = {
-        success_symbol = "[>](bold green)";
-        error_symbol = "[>](bold red)";
-      };
-    };
   };
 }
