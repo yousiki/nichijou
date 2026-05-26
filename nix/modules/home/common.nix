05160fdf462ade9c18b173a01d650547c1739a35
yousiki <you.siki@outlook.com>
2026-05-26 18:25:30 +0900

    home: disable catppuccin starship integration

--- nix/modules/home/common.nix
+++ nix/modules/home/common.nix
@@ -10,6 +10,9 @@
   catppuccin = {
     enable = true;
     flavor = "mocha";
+
+    # Starship imports a generated Catppuccin TOML file during evaluation.
+    starship.enable = false;
   };
 
   programs.home-manager.enable = true;
