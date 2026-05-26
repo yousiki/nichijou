3671191b6f6a69ec608be8266d35a56b3a4441b5
yousiki <you.siki@outlook.com>
2026-05-26 17:59:22 +0900

    home: enable catppuccin mocha

--- nix/modules/home/common.nix
+++ nix/modules/home/common.nix
@@ -1,7 +1,16 @@
-{ ... }:
+{ inputs, ... }:
 
 {
+  imports = [
+    inputs.catppuccin.homeModules.catppuccin
+  ];
+
   home.enableNixpkgsReleaseCheck = false;
+
+  catppuccin = {
+    enable = true;
+    flavor = "mocha";
+  };
 
   programs.home-manager.enable = true;
 }
