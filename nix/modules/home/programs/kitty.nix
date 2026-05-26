31681d5a46e412273feff27146021ce4b88ee12f
yousiki <you.siki@outlook.com>
2026-05-26 18:12:01 +0900

    home: use kitty shell integration option

--- nix/modules/home/programs/kitty.nix
+++ nix/modules/home/programs/kitty.nix
@@ -10,13 +10,14 @@
       size = 14;
     };
 
+    shellIntegration.mode = "enabled";
+
     settings = {
       macos_option_as_alt = "both";
       macos_quit_when_last_window_closed = "yes";
       confirm_os_window_close = 0;
       window_padding_width = 6;
       hide_window_decorations = "titlebar-only";
-      shell_integration = "enabled";
       copy_on_select = "clipboard";
       scrollback_lines = 10000;
       enable_audio_bell = false;
