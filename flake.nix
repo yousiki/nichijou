46325b6fe06a4ba86591a07df815fd3d9139d2ce
yousiki <you.siki@outlook.com>
2026-05-27 11:00:23 +0900

    flake: add nix-index database input

--- flake.nix
+++ flake.nix
@@ -19,6 +19,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     catppuccin = {
       url = "github:catppuccin/nix";
       inputs.nixpkgs.follows = "nixpkgs";
