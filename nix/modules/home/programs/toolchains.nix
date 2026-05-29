5b88bd3205b2d50976801319f3d51257fc18a890
yousiki <you.siki@outlook.com>
2026-05-30 02:34:49 +0900

    home: configure bun install path

--- nix/modules/home/programs/toolchains.nix
+++ nix/modules/home/programs/toolchains.nix
@@ -1,10 +1,21 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  bunInstall = "${config.home.homeDirectory}/.bun";
+in
 {
   programs.bun = {
     enable = true;
     package = pkgs.bun;
   };
+
+  home.sessionVariables = {
+    BUN_INSTALL = bunInstall;
+  };
+
+  home.sessionPath = [
+    "${bunInstall}/bin"
+  ];
 
   home.packages = [
     pkgs.nodejs_24
